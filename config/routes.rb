90bff18439e616926bd9798ea9859e28242ef227
Ron <rhoesada@gmail.com>
2014-03-04 21:45:02 -0800

    Localization

--- config/routes.rb
+++ config/routes.rb
@@ -10,18 +10,17 @@ Depot::Application.routes.draw do
   get "sessions/destroy"
   resources :users
 
-  resources :orders
-
-  resources :line_items
-
-  resources :carts
-
   get "store/index"
   resources :products do
     get :who_bought, on: :member
   end
 
-  root 'store#index', as: 'store'
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store'
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
